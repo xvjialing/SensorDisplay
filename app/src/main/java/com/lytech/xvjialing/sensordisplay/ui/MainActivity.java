6ef5a78257d5c9fb8d297405a721d7e468dbc7be
xvjialing <xvjialing@outlook.com>
2017-08-23 08:50:19 +0800

    完成登陆注册界面

---
+++ app/src/main/java/com/lytech/xvjialing/sensordisplay/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lytech.xvjialing.sensordisplay.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lytech.xvjialing.sensordisplay.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
